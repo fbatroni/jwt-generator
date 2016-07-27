42d1b86f076bd29d8896d85afeb454d92ff78bd2
Amit Segal <asegal@gmail.com>
2016-07-27 15:38:58 -0400

    deleting node_modules

--- node_modules/glg-jwt/tests/runall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "./lib" ]; then
-  for file in $(find "./lib" -type f -name "*.js"); do
-    echo "------------------------------------"
-    echo "Running Tests for: ${file}"
-    node "${file}"
-  done
-fi
